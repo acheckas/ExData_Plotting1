ba04511c39696871397cc450a089f877c6718a99
Anthony Checkas <acheckas@Elysium.home>
2023-02-12 19:17:01 -0700

    Initial commit of Course Project 1

---
+++ plot3.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+loadData <- function() {
+  #
+  # Note: you'll need to set your working directory to the directory that contains the following text file
+  #
+  full_consumption <- read.delim("household_power_consumption.txt", sep=";")
+  ## Convert the date first to make it easier to subset the table
+  full_consumption$Date <- dmy(full_consumption$Date)
+  ## Pull our subset out and then release the full set
+  feb_consumption <- full_consumption %>% filter(Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
+  rm(full_consumption)
+  # Now, let's make a real date + time field and drop the date and time columns
+  feb_consumption$DateTime <- ymd_hms(paste(feb_consumption$Date,feb_consumption$Time))
+  feb_consumption <- subset(feb_consumption,select=-c(Date,Time))
+  # Clean up all the character values so they're numeric
+  feb_consumption$Global_active_power <- as.numeric(feb_consumption$Global_active_power)
+  feb_consumption$Global_reactive_power <- as.numeric(feb_consumption$Global_reactive_power)
+  feb_consumption$Voltage <- as.numeric(feb_consumption$Voltage)
+  feb_consumption$Global_intensity <- as.numeric(feb_consumption$Global_intensity)
+  feb_consumption$Sub_metering_1 <- as.numeric(feb_consumption$Sub_metering_1)
+  feb_consumption$Sub_metering_2 <- as.numeric(feb_consumption$Sub_metering_2)
+
+  invisible(feb_consumption)
+}
+
+
+feb_data <- loadData()
+png(filename= "./plot3.png", width=480, height=480)
+plot(feb_data$DateTime,feb_data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+points(feb_data$DateTime,feb_data$Sub_metering_2, type="l",col="red")
+points(feb_data$DateTime,feb_data$Sub_metering_3, type="l",col="blue")
+legend("topright",col=c("black","red","blue"), pch = c(NA,NA,NA), lty = c(1,1,1), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
